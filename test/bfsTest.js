1ebd0990db818c178c2f4a0869eb4d06da236a7e
sendo83 <iovel.fessha@gmail.com>
2017-09-07 19:30:59 +0200

    add bfs test

---
+++ test/bfsTest.js
@@ -0,0 +1,27 @@
+const assert = require("assert");
+const algorithm = require("../algorithm/bfs");
+
+let graph = [
+  [1, 1, 0, 0, 1, 0],
+  [1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 0],
+  [0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 0, 0],
+  [0, 0, 0, 1, 0, 0]
+];
+
+describe("Algorithm", function() {
+  describe("#bfs()", function() {
+    it("Given the graph, path beteween node 1 and node 4 should be [1,2,3,5]", function() {
+      assert.deepStrictEqual([1, 2, 3, 5], algorithm.bfs(graph, 1, 5));
+    });
+  });
+
+  describe("#computePath()", function() {
+    it("Given the parents sets and target node equal to 4 the path should be [2,1,4] ", function() {
+      let parents = [ 1, 2, null, 2, 1, 3 ];
+      let targetNode = 4;
+      assert.deepStrictEqual([2, 1, 4], algorithm.computePath(parents, targetNode));
+    });
+  });
+});
