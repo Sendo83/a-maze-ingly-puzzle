38ef76e4449f7c214f7fbcf86ad123c83f4b7ba1
sendo83 <iovel.fessha@gmail.com>
2017-09-07 16:48:47 +0200

    add methods to create and remote test file

---
+++ test/createTestFile.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+const createTestMapFile = function() {
+  let testMaze = '{"rooms": [{ "id": 1, "name": "Hallway", "north": 2, "objects": [] }, { "id": 2, "name": "Dining Room", "south": 1, "west": 3, "east": 4, "objects": [] }, { "id": 3, "name": "Kitchen","east":2, "objects": [ { "name": "Knife" } ] }, { "id": 4, "name": "Sun Room","west":2, "objects": [ { "name": "Potted Plant" } ] }]}';
+  fs.writeFile("./map.json", function(err){
+    if(err) return err
+    console.log("File created successfully")
+  })
+}
+
+const deleteTestMapFile = function() {
+  fs.unlink("./map.json", function(err){
+    if (err) return err
+    console.log("File deleted successfully")
+  })
+}
+
+module.exports = {
+  createTestMapFile : createTestMapFile,
+  deleteTestMapFile : deleteTestMapFile
+}
