7d5e1379c02d270d7f068e0930dfcfae4eb1b73e
sendo83 <iovel.fessha@gmail.com>
2017-09-09 15:10:20 +0200

    add test cases for validator

---
+++ test/validatorTest.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const assert = require("assert");
+const validate = require("../utils/validator");
+
+const startRoom = 2;
+const toCollect = ["Knife", "Potted Plant"];
+const object = "Knife";
+const objectsMap = { Knife: 3, "Potted Plant": 4 };
+const roomsMap = [
+  {},
+  { id: 1, name: "Hallway", north: 2, objects: [] },
+  { id: 2, name: "Dining Room", south: 1, west: 3, east: 4, objects: [] },
+  { id: 3, name: "Kitchen", east: 2, objects: [{ name: "Knife" }] },
+  { id: 4, name: "Sun Room", west: 2, objects: [{ name: "Knife" }] }
+];
+
+describe("Validator", function() {
+  describe("#startRoom()", function() {
+    it("should execute without error if start room is inside the map", function() {
+      validate.startRoom(startRoom, roomsMap);
+      assert.equal(null, process.exitCode);
+    });
+  });
+  describe("#objectsToCollect()", function() {
+    it("should execute without error if test objects to collect are inside the map", function() {
+      validate.objectsToCollect(toCollect, objectsMap);
+      assert.equal(null, process.exitCode);
+    });
+  });
+  describe("#isObjectValid()", function() {
+    it("should execute without error it test objct is inside map", function() {
+      validate.isObjectValid(object, objectsMap);
+      assert.equal(null, process.exitCode);
+    });
+  });
+});
