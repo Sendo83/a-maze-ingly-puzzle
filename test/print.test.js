c982a1dcafb1e639ce932fc19386ea819423ab57
sendo83 <iovel.fessha@gmail.com>
2017-09-10 12:24:55 +0200

    add a test for print module

--- test/print.test.js
+++ test/print.test.js
@@ -1,7 +1,22 @@
 "use strict";
 
 const assert = require("assert");
-const rp = require("../route/routePlanner");
+const print = require("../utils/print");
 
-const roomsMap = null;
-const toCollect = ["Knife", "Potted Plant"]
+const route = [2, 4, 2, 3];
+const roomsMap = [
+  {},
+  { id: 1, name: "Hallway", north: 2, objects: [] },
+  { id: 2, name: "Dining Room", south: 1, west: 3, east: 4, objects: [] },
+  { id: 3, name: "Kitchen", east: 2, objects: [{ name: "Knife" }] },
+  { id: 4, name: "Sun Room", west: 2, objects: [{ name: "Potted Plant" }] }
+];
+const toCollect = ["Knife", "Potted Plant"];
+
+describe("Print", function() {
+  describe("#printCollectedObjects()", function() {
+    it("should execute without error, eg returning 0", function() {
+      assert.equal(0, print.printResult(route, roomsMap, toCollect));
+    });
+  });
+});
